08c723e060d8780a43ba69c49b83e407641a7de7
BaloghDarius <71815276+cszalan@users.noreply.github.com>
2022-09-15 15:58:39 +0200

    hibak commentezese

--- main.cpp
+++ main.cpp
@@ -4,20 +4,29 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
+    // Hiányzik az alulvonás
     int *b = new int[NELEMENTS];
+    // Nincsen pontosvessző a sor végén, rossz aposztóf használata
     std::cout << '1-100 ertekek duplazasa'
+    // hiányzik a léptetés
     for (int i = 0;)
     {
+        // 0-tól 99-ig dupláz... 1-től 100-ig kéne
         b[i] = i * 2;
     }
+    // a ciklus vezérlő hibás, mindig hamisra fut így nem lép be a ciklusba
     for (int i = 0; i; i++)
     {
+    // magát az értéket nem írjuk ki, illetve hiányzik a pontos vessző a sor végéről
         std::cout << "Ertek:"
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
+    //nincs kezdőértéke a változónak
     int atlag;
+    // hiányzik egy pontosvessző
     for (int i = 0; i < N_ELEMENTS, i++)
     {
+    // szintén hiányzik egy pontos vessző
         atlag += b[i]
     }
     atlag /= N_ELEMENTS;
