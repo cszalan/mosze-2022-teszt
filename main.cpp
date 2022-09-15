21e3b2ba31df636ac8a8af7b138a85c9686de96c
BaloghDarius <71815276+cszalan@users.noreply.github.com>
2022-09-15 15:59:29 +0200

    hibak javitasa

--- main.cpp
+++ main.cpp
@@ -5,31 +5,32 @@ constexpr int N_ELEMENTS = 100;
 int main()
 {
     // Hiányzik az alulvonás
-    int *b = new int[NELEMENTS];
+    int *b = new int[N_ELEMENTS];
     // Nincsen pontosvessző a sor végén, rossz aposztóf használata
-    std::cout << '1-100 ertekek duplazasa'
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
     // hiányzik a léptetés
-    for (int i = 0;)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        // 0-tól 99-ig dupláz... 1-től 100-ig kéne
-        b[i] = i * 2;
+    // 0-tól 99-ig dupláz... 1-től 100-ig kéne
+        b[i] = (i + 1) * 2;
     }
     // a ciklus vezérlő hibás, mindig hamisra fut így nem lép be a ciklusba
-    for (int i = 0; i; i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
     // magát az értéket nem írjuk ki, illetve hiányzik a pontos vessző a sor végéről
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" << b[i] << std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     //nincs kezdőértéke a változónak
-    int atlag;
+    int atlag = 0;
     // hiányzik egy pontosvessző
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
     // szintén hiányzik egy pontos vessző
-        atlag += b[i]
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+
     return 0;
 }
